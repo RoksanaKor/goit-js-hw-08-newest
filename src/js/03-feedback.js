9aa13ac64ab8c39442c53d9f24356a0ca2afe9a9
RoksanaKor <roksanakorban@gmail.com>
2024-02-26 15:30:46 +0100

    Clone form from someone who told it works, it does not.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,108 @@
-import _ from 'lodash';
+// import { _ } from 'lodash';
 
+// const form = document.querySelector('.feedback-form');
+// const localStorageKey = 'feedback-form-state';
+// const formEmail = document.querySelector("[name='email']");
+// const formMessage = document.querySelector("[name='message']");
+
+// function saveInput() {
+//   const email = formEmail.value.trim();
+//   const message = formMessage.value.trim();
+//   const formData = {
+//     email,
+//     message,
+//   };
+// }
+
+// try {
+//   const stringedData = JSON.stringify(formData);
+//   localStorage.setItem(localStorageKey, stringedData);
+// } catch (error) {
+//   console.log('stringify error:' + error);
+// }
+
+// const throttleSaveInput = _.throttle(saveInput, 500);
+
+// formEmail.addEventListener('input', throttleSaveInput);
+// formMessage.addEventListener('input', throttleSaveInput);
+// form.addEventListener('submit', onSubmit);
+
+// function onSubmit(event) {
+//   event.preventDefault();
+//   const sentData = {
+//     email: formEmail.value.trim(),
+//     message: formMessage.value.trim(),
+//   };
+//   if (sentData.email === '' || sentData.message === '') {
+//     alert('Fill all the fields.');
+//   } else {
+//     console.log(sentData);
+//     localStorage.removeItem(localStorageKey);
+//     formEmail.value = null;
+//     formMessage.value = null;
+//   }
+// }
+
+// try {
+//   const parsedData = JSON.parse(localStorage.getItem(localStorageKey));
+//   if (parsedData.email !== '' || parsedData.message !== '') {
+//     formEmail.value = parsedData.email;
+//     formMessage.value = parsedData.message;
+//   }
+// } catch (error) {
+//   console.log('parse error:' + error);
+// }
+
+import { _ } from 'lodash';
+
+const emailInput = document.querySelector("[name='email']");
+const messageInput = document.querySelector("[name='message']");
 const form = document.querySelector('.feedback-form');
-const localStorageKey = 'feedback-form-state';
 
-// const object = {
-//   email: form.elements.email.value,
-//   message: form.elements.message.value,
-// };
-// const dataJSON = JSON.stringify(object);
+function saveInput() {
+  const email = emailInput.value.trim();
+  const message = messageInput.value.trim();
+  const data = {
+    email,
+    message,
+  };
 
-form.elements.email.value = localStorage.getItem(localStorageKey) ?? '';
+  try {
+    const stringedData = JSON.stringify(data);
+    localStorage.setItem('feedback-form-state', stringedData);
+  } catch (error) {
+    console.log('stringedData error: ' + error);
+  }
+}
 
-form.elements.message.value = localStorage.getItem(localStorageKey) ?? '';
+const throttleSave = throttle(saveInput, 500);
 
-form.elements.email.addEventListener(
-  'input',
-  _.throttle(event => {
-    localStorage.setItem(localStorageKey, event.target.value);
-  }, 500)
-);
+emailInput.addEventListener('input', throttleSave);
+messageInput.addEventListener('input', throttleSave);
+form.addEventListener('submit', onSubmit);
 
-form.elements.message.addEventListener(
-  'input',
-  _.throttle(event => {
-    localStorage.setItem(localStorageKey, event.target.value);
-  }, 500)
-);
+function onSubmit(event) {
+  event.preventDefault();
+  const sentData = {
+    email: emailInput.value.trim(),
+    message: messageInput.value.trim(),
+  };
+  if (sentData.email === '' || sentData.message === '') {
+    alert('Both fields must be filled before sending');
+  } else {
+    console.log(sentData);
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = null;
+    messageInput.value = null;
+  }
+}
 
-form.addEventListener(
-  'submit',
-  _.throttle(event => {
-    event.preventDefault();
-    console.log(localStorage.getItem(localStorageKey));
-    localStorage.removeItem(localStorageKey);
-    form.elements.email.value = '';
-    form.elements.message.value = '';
-  }, 500)
-);
+try {
+  const parsedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (parsedData.email !== '' || parsedData.message !== '') {
+    emailInput.value = parsedData.email;
+    messageInput.value = parsedData.message;
+  }
+} catch (error) {
+  console.log('parsedData error: ' + error);
+}
